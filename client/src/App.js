798c0453c6234b09eab4b5104417176f8f98a51b
Dhruv Bahl <dbahl@csuchico.edu>
2024-07-15 14:16:30 -0700

    Update - Delete- Add mutations have been added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
 import Clients from "./components/Clients";
+import Projects from "./components/Projects";
+import AddClientModal from "./components/AddClientModal";
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming){
+            return incoming
+          }
+        },
+        projects: {
+          merge(existing, incoming){
+            return incoming
+      },
+    },
+  },
+ },
+}
+}); 
 
 const client = new ApolloClient({
   uri: 'http://localhost:5001/graphql',
-  cache: new InMemoryCache(),
+  cache,
 });
 function App() {
   return (
@@ -12,6 +33,8 @@ function App() {
     <ApolloProvider client={client}>
     <Header />
     <div className="container">
+      <AddClientModal/>
+      <Projects/> 
       < Clients />
     </div>
     </ApolloProvider>
